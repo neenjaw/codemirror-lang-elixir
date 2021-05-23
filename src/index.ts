7e1c444136b17d591b7d0c7ffb29c8ec01be868a
Tim Austin <tim@neenjaw.com>
2021-05-23 10:23:13 -0600

    rename to elixir

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { parser } from './syntax.grammar'
+import {
+  LezerLanguage,
+  LanguageSupport,
+  indentNodeProp,
+  foldNodeProp,
+  foldInside,
+  delimitedIndent,
+} from '@codemirror/language'
+import { styleTags, tags as t } from '@codemirror/highlight'
+
+export const ElixirLanguage = LezerLanguage.define({
+  parser: parser.configure({
+    props: [
+      indentNodeProp.add({
+        Application: delimitedIndent({ closing: ')', align: false }),
+      }),
+      foldNodeProp.add({
+        Application: foldInside,
+      }),
+      styleTags({
+        Identifier: t.variableName,
+        Boolean: t.bool,
+        String: t.string,
+        LineComment: t.lineComment,
+        '( )': t.paren,
+      }),
+    ],
+  }),
+  languageData: {
+    commentTokens: { line: ';' },
+  },
+})
+
+export function Elixir() {
+  return new LanguageSupport(ElixirLanguage)
+}
